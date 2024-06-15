dafce92e58ed4f7f42b6b1adb4d02cb6d35c04c1
ali <ahmmsel2@gmail.com>
2024-06-16 00:17:19 +0300

    Fix auth problem

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -2,16 +2,31 @@ import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
+import { authOptions } from "@/lib/auth";
 
 export async function getCurrentUser() {
-	const session = await getServerSession();
+	const session = await getServerSession(authOptions);
 
-	const user = await db.user.findFirst({
-		where: { email: session?.user?.email as string },
-	});
+	if (!session || !session?.user || !session?.user?.email) {
+		return redirect("/auth/sign-in");
+	}
+
+	const user = await getUserByEmail(session.user.email);
 
 	return user;
 }
+
+export const getUserByEmail = async (email: string) => {
+	try {
+		const user = await db.user.findUnique({
+			where: { email },
+		});
+
+		return user;
+	} catch {
+		return null;
+	}
+};
 
 export async function getSettings() {
 	const user = await getCurrentUser();
